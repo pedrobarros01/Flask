bca22cbcf29c2a80f780a9a51840df9e35d6f13c
ImperadorPombinho <pedrobarros0101@outlook.com>
2021-10-19 12:42:55 -0300

    [´FEAT][FLASK]
    fazendo o get e o post da classe hotel

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_restful import Api
-from resources.hotel import Hoteis
+from resources.hotel import Hoteis, Hotel
 app = Flask(__name__)
 api = Api(app)
 
 
 
 api.add_resource(Hoteis, '/hoteis')
+api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
 
 #http://127.0.0.1:5000/hoteis -> lista de hoteis 
 
--- resources/hotel.py
+++ resources/hotel.py
@@ -1,21 +1,21 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 hoteis = [
     {
-        'hotelId': 'alpha',
+        'hotel_id': 'alpha',
         'nome': 'AlphaHotel',
         'estrelas': 4.3,
         'diaria': 420.34,
         'cidade': 'Rio de Janeiro'
     },
     {
-        'hotelId': 'bravo',
+        'hotel_id': 'bravo',
         'nome': 'BravoHotel',
         'estrelas': 4.4,
         'diaria': 380.90,
         'cidade': 'Santa Catarina'
     },
     {
-        'hotelId': 'charlie',
+        'hotel_id': 'charlie',
         'nome': 'CharlieHotel',
         'estrelas': 3.9,
         'diaria': 320.20,
@@ -25,3 +25,36 @@ hoteis = [
 class Hoteis(Resource):
     def get(self):
         return {'hoteis': hoteis}
+
+
+class Hotel(Resource):
+    def get(self, hotel_id):
+        for hotel in hoteis:
+            if hotel['hotel_id'] == hotel_id:
+                return hotel
+        return {'message': 'Hotel not found'}, 404 #nao econtrado
+
+    def post(self, hotel_id):
+        argumentos = reqparse.RequestParser()
+        argumentos.add_argument('nome')
+        argumentos.add_argument('estrelas')
+        argumentos.add_argument('diaria')
+        argumentos.add_argument('cidade')
+        
+        dados = argumentos.parse_args()
+
+        novo_hotel = {
+            'hotel_id': hotel_id,
+            'nome': dados['nome'],
+            'estrelas': dados['estrelas'],
+            'diaria': dados['diaria'],
+            'cidade': dados['cidade']
+        }
+        hoteis.append(novo_hotel)
+        return novo_hotel, 200 #sucesso
+    
+    def put(self, hotel_id):
+        pass
+    
+    def delete(self, hotel_id):
+        pass
